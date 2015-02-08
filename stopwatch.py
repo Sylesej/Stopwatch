9b27e8df362990a018dc3263ebd49f5adbee83f7
Nils <nils.toudal@gmail.com>
2015-02-08 11:16:05 +0100

    Splitted analyze in analyze and loadtrack to make a dict over zones. Implimented basic user feedback in analyze

--- stopwatch.py
+++ stopwatch.py
@@ -10,22 +10,21 @@ import linecache
 import os
 
 def start():
-  #This function defines the start time if the user chooses to.
-  goOn = False
-
-  while goOn!=True:
-    inp = raw_input('Enter S for Start or E for Exit: ')
-    if inp == 'S':
-      tStart = time.time()
-      goOn = True
-    elif inp == 'E':
-      print 'You terminated the program.'
-      print 'Goodbye\n'
-      sys.exit()
-    else:
-      print 'Your input was not accepted.'
-      print 'Please enter a valid input.'
-
+    goOn = False
+    #This function defines the start time if the user chooses to.
+    while goOn!=True:
+        inp = raw_input('Enter S for Start or E for Exit: ')
+        if inp == 'S':
+            tStart = time.time()
+            goOn = True
+        elif inp == 'E':
+            print 'You terminated the program.'
+            print 'Goodbye\n'
+            sys.exit()
+        else:
+            print 'Your input was not accepted.'
+            print 'Please enter a valid input.'
+            goOn = False
     return tStart
 
 def lapTime(startTime): #, data
@@ -61,8 +60,43 @@ def load_track(track):
         zones[n] = zones[n].split(' ',2)
     return zones
 
-def analyze(zones,data,trackfile):
-    print 'I analyze stuff.'
+def analyze(data,zonedict):
+    #Read file from beginning, file is read on every itteration of while loop.
+    #Not super elegant.
+    data.seek(0,0)
+    #Read data into content, to treat as a str
+    content = data.read()
+    #Split data into each line, will output a 2d array
+    splits = content.splitlines()
+    times = [0]*len(splits)
+    sector = [0]*len(splits)
+    for n in range(len(splits)):
+        #Convert to string to use string methods. Back to 1d array.
+        splits[n] = str(splits[n])
+        #Split by space, into 2d again
+        splits[n] = splits[n].split()
+        #Time is first, then sector indicator.
+        times[n] = splits[n][0]
+        sector[n] = splits[n][1]
+        #Print for confirmation
+        print 'Line: ' +str(n+1) + ' Time: ' +times[n] + ' Sector: ' +sector[n]
+        print 'Sector length: ' + str(zonedict[n+1][0])
+        print 'Expected time: ' + str(zonedict[n+1][1]) + '\n'
+
+
+def loadtrack(trackfile):
+    track.seek(0,0)
+    #Read data into content, to treat as a str
+    tcontent = track.read()
+    tcontent = str(tcontent)
+    tcontent = tcontent.split('zones:\n')
+    tcontent = tcontent[1]
+    tcontent = tcontent.split()
+    zonedict = {}
+    for n in range(len(tcontent)/2):
+        zonedict[n+1] = [int(tcontent[n*2]),int(tcontent[n*2+1])]
+    print zonedict
+    return zonedict
 
 
 
@@ -74,8 +108,8 @@ mapfile = linecache.getline('trackdata.txt',4)
 mapfile = mapfile[0:len(mapfile)-1]
 
 #Files are opened
-data = open(filename,'w')
-track = open(trackfile,'a')
+data = open(filename,'w+')
+track = open(trackfile,'r')
 themap = open(mapfile,'r')
 
 #Before printing anything, clear terminal
@@ -95,11 +129,15 @@ for obj in zones:
     a = a + 1
 print '\n'
 
+#Zones area loaded from trackfile
+zonedict = loadtrack(trackfile)
+
 #Time is started by user command
 tStart = start()
 
 #Race is going on as long as the user wants
 race = 'go'
+entries = 1
 os.system('clear')
 while race != False:
     themap = open(mapfile,'r')
@@ -107,7 +145,7 @@ while race != False:
     themap.close()
     #Analyze data on every run exept the first
     if race == True:
-        analyze(zones,data,trackfile)
+        analyze(data,zonedict)
     race = lapTime(tStart)
     #Clear terminal
     os.system('clear')
